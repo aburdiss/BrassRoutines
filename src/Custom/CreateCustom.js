339031299c3593b275660ffb42510885b8de114e
Alexander Burdiss <aburdiss@icloud.com>
2021-01-16 13:21:30 -0500

    Added Landscape mode for create custom view.

--- src/Custom/CreateCustom.js
+++ src/Custom/CreateCustom.js
@@ -1,10 +1,18 @@
 import React, {useContext, useEffect, useState} from 'react';
-import {View, TextInput, Text, Alert, Pressable} from 'react-native';
+import {
+  View,
+  TextInput,
+  Text,
+  Alert,
+  Pressable,
+  Dimensions,
+} from 'react-native';
 import {
   DynamicStyleSheet,
   DynamicValue,
   useDynamicValue,
 } from 'react-native-dynamic';
+import SafeAreaView from 'react-native-safe-area-view';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import DraggableFlatList from 'react-native-draggable-flatlist';
 import {useNavigation, useRoute} from '@react-navigation/native';
@@ -25,13 +33,13 @@ import {translate} from '../Translations/TranslationModel';
  * in memory, and save the current routine in its place.
  * @author Alexander Burdiss
  * @since 1/2/21
- * @version 1.0.0
+ * @version 1.1.0
  *
  * @component
  * @example
- *   <CreateCustom />
- *
- * @todo Create a landscape view for this component.
+ * ```jsx
+<CreateCustom />
+```
  */
 const CreateCustom = () => {
   const styles = useDynamicValue(dynamicStyles);
@@ -44,18 +52,21 @@ const CreateCustom = () => {
   const [counter, setCounter] = useState(0);
   const [editMode, setEditMode] = useState(false);
   const [routineCurrentIndex, setRoutineCurrentIndex] = useState(undefined);
+  const [isPortrait, setIsPortrait] = useState(true);
   useEffect(
     /**
-     * @function CreateCustom~useEffect~checkIfEditRoutine
-     * @summary Sets component up for edit mode.
+     * @function CreateCustom~useEffect~setupComponent
+     * @summary Sets component up for edit mode, and checks orientation.
      * @description Checks to see if there are params passed in, and if so
      * sets the params passed in as the current routine, and sets the variables
-     * necessary to edit the routine instead of create a new one.
+     * necessary to edit the routine instead of create a new one. This funciton
+     * also checks the orientation on mount and sets the state variable with
+     * that information.
      * @author Alexander Burdiss
      * @since 1/2/21
      * @version 1.0.0
      */
-    function checkIfEditRoutine() {
+    function setupComponent() {
       if (route.params != undefined) {
         setEditMode(true);
         setRoutineCurrentIndex(route.params.index);
@@ -68,8 +79,38 @@ const CreateCustom = () => {
           title: translate('Edit Routine'),
         });
       }
+      setIsPortrait(getIsPortrait());
     },
     [],
+  );
+
+  /**
+   * @function CreateCustom~getIsPortrait
+   * @description Gets whether or not the component is in portrait mode.
+   * @author Alexander Burdiss
+   * @since 1/16/21
+   * @version 1.0.0
+   * @returns {Boolean} A boolean of whether or not the screen is in portrait
+   * mode.
+   */
+  const getIsPortrait = () => {
+    const dim = Dimensions.get('screen');
+    return dim.height >= dim.width;
+  };
+
+  Dimensions.addEventListener(
+    'change',
+    /**
+     * @function CreateCustom~Dimensions.addEventListener~updatePortraitMode
+     * @description Updates portrait mode state variable after calculating if the
+     * device is in portrait mode.
+     * @author Alexander Burdiss
+     * @since 1/16/21
+     * @version 1.0.0
+     */
+    function updatePortraitMode() {
+      setIsPortrait(getIsPortrait());
+    },
   );
 
   /**
@@ -159,7 +200,7 @@ const CreateCustom = () => {
     setCurrentRoutine(data);
   }
 
-  return (
+  return isPortrait ? (
     <View style={styles.container}>
       <TextInput
         value={routineName}
@@ -233,6 +274,96 @@ const CreateCustom = () => {
         text={editMode ? 'Save' : 'Create'}
       />
     </View>
+  ) : (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: new DynamicValue(
+          colors.systemGray6Light,
+          colors.black,
+        ),
+      }}>
+      <View style={{width: '50%'}}>
+        <ExercisePicker
+          selectedExercise={selectedExercise}
+          setSelectedExercise={setSelectedExercise}
+        />
+        <View style={{flex: 1}} />
+        <View style={styles.buttonContainer}>
+          <ResetButton handler={removeAllExercises} />
+          <AddToListButton handler={addToExerciseList} />
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+        }}>
+        <TextInput
+          value={routineName}
+          placeholder={translate('Routine Name')}
+          onChangeText={(text) => setRoutineName(text)}
+          style={styles.textInput}
+        />
+        <DraggableFlatList
+          style={styles.landscapeList}
+          data={currentRoutine}
+          keyExtractor={(item, index) => String(index)}
+          onDragEnd={setNewRoutineOrder}
+          renderItem={({item, drag}) => (
+            <SwipeableRow
+              key={`${item}${counter}`}
+              styles={styles}
+              delete={deleteElement}
+              item={item}>
+              <Pressable
+                onPress={() => {
+                  navigation.navigate('Exercise Detail', {
+                    instrument: state.instrument,
+                    item: item,
+                  });
+                }}
+                accessibilityRole="link"
+                accessibilityHint={translate('Opens Exercise')}
+                accessibilityLabel={
+                  getExerciseDisplayName(item, state) +
+                  (state.favorites.includes(item)
+                    ? translate('This is a favorite exercise')
+                    : '')
+                }
+                onLongPress={drag}
+                style={({pressed}) => ({opacity: pressed ? 0.7 : 1})}>
+                <View style={styles.listItemContainer}>
+                  <View style={styles.listItemTextContainer}>
+                    {state.favorites.includes(item) ? (
+                      <Ionicons
+                        accessibilityLabel={translate(
+                          'This is a favorite exercise',
+                        )}
+                        name={'heart'}
+                        color={styles.listIcon.color}
+                        size={20}
+                        style={{paddingRight: 5}}
+                      />
+                    ) : null}
+                    <Text style={styles.listItemText}>
+                      {getExerciseDisplayName(item, state)}
+                    </Text>
+                  </View>
+                </View>
+              </Pressable>
+            </SwipeableRow>
+          )}
+        />
+        <MainActionButton
+          accessibilityLabel={
+            editMode ? translate('Save Routine') : translate('Create Routine')
+          }
+          handler={createRoutine}
+          text={editMode ? 'Save' : 'Create'}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -248,6 +379,11 @@ const dynamicStyles = new DynamicStyleSheet({
   },
   list: {
     flex: 1,
+  },
+  landscapeList: {
+    flex: 1,
+    borderRadius: 8,
+    marginHorizontal: 10,
   },
   listIcon: {
     color: new DynamicValue(colors.pinkLight, colors.pinkDark),
