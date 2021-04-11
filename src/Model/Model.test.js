0b472035f23c10d187a497a6ba39186e5da517a4
Alexander Burdiss <aburdiss@icloud.com>
2021-04-10 23:28:33 -0400

    Updated Model Tests to check all links for 200 responses

--- src/Model/Model.test.js
+++ src/Model/Model.test.js
@@ -1,6 +1,12 @@
 import 'react-native';
 import React from 'react';
 
+const fetch = require('node-fetch');
+const https = require('https');
+const httpsAgent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
 import {
   allHornExercises,
   allTrumpetExercises,
@@ -85,12 +91,32 @@ test.each(allTubaExercises)('tuba exercises have path', (item) => {
   expect(duplicatedItem).not.toBeTruthy();
 })();
 
-// it('has all valid links', async () => {
-//   expect.assertions(RESOURCES.length);
+describe('resource links', () => {
+  RESOURCES.map(async (resource) => {
+    if (!['13C'].includes(resource.id)) {
+      test(resource.value, async () => {
+        if (resource.type == 'link' && resource.link.startsWith('http')) {
+          const resp = await fetch(resource.link, {
+            method: 'GET',
+            agent: httpsAgent,
+          });
+          expect(resp.status).toEqual(200);
+        }
+      });
+    }
+  });
+});
 
-//   console.log(RESOURCES);
-//   for (let item of RESOURCES) {
-//     let response = await fetch(item.link);
-//     expect(response.status).toBe('200');
-//   }
-// });
+describe('about links', () => {
+  ABOUT.map(async (about) => {
+    test(about.value, async () => {
+      if (about.type == 'link' && about.link.startsWith('http')) {
+        const resp = await fetch(about.link, {
+          method: 'GET',
+          agent: httpsAgent,
+        });
+        expect(resp.status).toEqual(200);
+      }
+    });
+  });
+});
